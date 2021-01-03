3aaa08c84e3dbcaea910e53b76985b63467548ae
k <k@localhost.localdomain>
2021-01-04 01:31:05 +0200

    remade task

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Список фильмов на странице сформировать на основании данных из этого JS
+файла.Отсортировать их по алфавиту 
 
 5) Добавить нумерацию выведенных фильмов */
 
@@ -24,3 +24,18 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelector('.promo__adv');
+const genre = document.querySelector('.promo__genre');
+const bg = document.querySelector('.promo__bg');
+const list = document.querySelector('.promo__interactive-list');
+
+adv.style.display = "none";
+genre.textContent = "Драма";
+bg.style.backgroundImage = "url(../img/bg.jpg)";
+
+list.innerHTML = "";
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    list.innerHTML += `<li class="promo__interactive-item">${i+1} ${film}
+    <div class="delete"></div>`
+});
